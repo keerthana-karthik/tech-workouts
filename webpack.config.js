aca24f933999d646be32597a8a78ce5a0d660216
subramaniashiva <vengaishiva@gmail.com>
2016-04-01 00:24:32 +0200

    Changed the context to src. Updated to use sass loader for scss files

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,23 @@
 var path = require('path');
 
 module.exports = {
+  context: __dirname + '/src',
   entry: {
-    javascript: "./index.js",
-    html: "./index.html"
+    javascript: './index.js',
+    html: './index.html'
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  resolve: {
+    root: [path.resolve(__dirname, 'src'), path.resolve(__dirname, 'node_modules')]
+  },
   module: {
     preLoaders: [
       {
         test: /\.js$/, 
-        loader: "eslint-loader", 
+        loader: 'eslint-loader', 
         exclude: /node_modules/
       }
     ],
@@ -21,14 +25,25 @@ module.exports = {
       { 
         test: /\.html/, 
         loader: 'file?name=[name].[ext]' 
-      },
-      {
+      }, {
+        test: /\.scss$/,
+        loader: 'style!css!sass'
+      }, {
         test: '/\.css/',
         loader: 'style!css'
       }, { 
         test: /\.js$/, 
         loaders: ['react-hot', 'babel-loader'], 
         exclude: /node_modules/ 
+      }, {
+        test: /\.(woff2?|svg)$/,
+        loader: 'url?limit=10000'
+      }, {
+        test: /\.(png|jpg)$/,
+        loader: 'url?limit=8192'
+      }, {
+        test: /\.(ttf|eot)$/,
+        loader: 'file'
       }
     ]
   }
