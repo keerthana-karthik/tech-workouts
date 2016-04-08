1a818c2d6d708ad59b1e4394055086d52939f236
subramaniashiva <vengaishiva@gmail.com>
2016-04-09 01:50:50 +0200

    Added workouts in the state tree

--- src/redux/reducers/root-reducer.js
+++ src/redux/reducers/root-reducer.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import child_1 from './child_1';
 import api_call from './api_call'
+import workouts from './workout-cards-reducer'
 
 export default combineReducers({
   child_1,
-  api_call
+  api_call,
+  workouts
 })
