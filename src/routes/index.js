31589a77b5633d0a7f1997dddb285b57420f6f44
subramaniashiva <vengaishiva@gmail.com>
2016-04-09 01:51:15 +0200

    Added route for workouts

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,14 +3,14 @@ import React from 'react'
 import { Route } from 'react-router'
 //Containers (layouts)
 import MainLayout from 'layouts/main'
+import WorkoutContainer from 'containers/workouts'
 import SocialMediaContainer from 'containers/socialMediaContainer'
-
-//<Route path='/' component={MainLayout}/>
 
 export default () => {
   return (
     <Route>
       <Route path='/' component={MainLayout} />
+      <Route path='/work-outs' component={WorkoutContainer} />
       <Route path='/social-media' component={SocialMediaContainer} />
     </Route>)
 }
