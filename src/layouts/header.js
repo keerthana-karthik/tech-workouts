dcaa7bd63bed1355aa1e451e94c94d17f5dad9ec
subramaniashiva <vengaishiva@gmail.com>
2016-04-09 02:46:20 +0200

    Added routes. Made main as the default layout

--- src/layouts/header.js
+++ src/layouts/header.js
@@ -1,20 +1,34 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 // Styles
 import 'styles/header.scss'
 
 const Header = ({ menus }) => (
-  <header className='header clearfix'>
-    <h3 className='pull-left logo'>
-      Tech Workouts
-    </h3>
-    <ul className='pull-right navbar'>
-      {menus && menus.map((x, index) => (
-       <li key = {index}>
-        <a href='#'>{x}</a>
-       </li>
-      ))}
-    </ul>
+  <header className = 'header clearfix'>
+    <nav className = 'navbar'>
+      <div className = 'container-fluid'>
+        <div className = 'navbar-header'>
+          <button type='button' className = 'navbar-toggle collapsed' data-toggle='collapse' data-target='#top-bar-menu' aria-expanded='false'>
+            <span className = 'sr-only'>Toggle navigation</span>
+            <span className = 'icon-bar'></span>
+            <span className = 'icon-bar'></span>
+            <span className = 'icon-bar'></span>
+          </button>
+          <a className = 'navbar-brand' href = 'javascript:void(0)'>Tech Workouts</a>
+        </div>
+
+        <div className = 'pull-right nav-bar' id = 'top-bar-menu'>
+          <ul className = 'nav navbar-nav'>
+            {menus && menus.map((item, index) => (
+             <li key = {index}>
+              <Link to = {item.href} activeClassName='active'>{item.name}</Link>
+             </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
 )
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,20 @@ import 'babel-polyfill'
 import React from 'react'
 import { Route } from 'react-router'
 //Containers (layouts)
-import MainLayout from 'layouts/main'
+import MainLayout from 'layouts/main']
+import WorkoutContainer from 'containers/workouts'
+import WorkoutTech from 'containers/WorkoutTech'
 import SocialMediaContainer from 'containers/socialMediaContainer'
 
 //<Route path='/' component={MainLayout}/>
 
 export default () => {
   return (
-    <Route>
-      <Route path='/' component={MainLayout} />
-      <Route path='/social-media' component={SocialMediaContainer} />
+    <Route>]
+      <Route path = '/' component = {MainLayout}>
+        <Route path = '/workouts' component = {WorkoutContainer} />
+        <Route path = '/workouts/:technology' component = {WorkoutTech} />
+        <Route path = '/social-media' component = {SocialMediaContainer} />
+      </Route>
     </Route>)
 }
