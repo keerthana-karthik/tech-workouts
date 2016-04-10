e37c6ed5b32d08d84f46e93d7aa96485ecf654e0
subramaniashiva <vengaishiva@gmail.com>
2016-04-11 00:19:36 +0200

    Updated API endpoints with github URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,5 @@
 export const API = {
-  ROOT_PATH: 'http://localhost:8080/data/',
+  ROOT_PATH: 'http://subramaniashiva.github.io/tech-workouts/dist/data/',
   END_POINTS: {
     GET_WORKOUTS: 'workouts.json'
   }
