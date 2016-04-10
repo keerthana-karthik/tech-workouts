4292e94dee9015e95935399775f8255aa8b13397
subramaniashiva <vengaishiva@gmail.com>
2016-04-10 23:47:26 +0200

    Updated root reducers to remove temp reducers

--- src/redux/reducers/root-reducer.js
+++ src/redux/reducers/root-reducer.js
@@ -1,13 +1,9 @@
 import { combineReducers } from 'redux';
 
-import child_1 from './child_1'
-import api_call from './api_call'
 import workouts from './workout-cards-reducer'
 import currentLesson from './workout-content-reducer'
 
 export default combineReducers({
-  child_1,
-  api_call,
   workouts,
   currentLesson
 })
